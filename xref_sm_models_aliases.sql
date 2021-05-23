2516a0fa54fd099a44c743a32cd8809c05afa44d
timofeykl <84674136+timofeykl@users.noreply.github.com>
2021-05-23 20:40:35 +0300

    Update xref_sm_models_aliases.sql

--- xref_sm_models_aliases.sql
+++ xref_sm_models_aliases.sql
@@ -1,8 +1,8 @@
 SELECT DISTINCT NULL AS model_alias_id,
   NULL               AS model_id,
-  nfo.model_code
-FROM STG_NFO.nf_purpose_detail_vehicle nfo
-LEFT JOIN DM_SM.xref_sm_models_aliases ma
+ !!!
+FROM !!!
+LEFT JOIN !!!
 ON upper(ma.model_alias) = upper(nfo.model_code)
 WHERE ma.model_alias    IS NULL
-ORDER BY NFO.MODEL_CODE
+ORDER BY !!!
