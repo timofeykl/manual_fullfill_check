b896fdde890a2f0735624fa206164bfb486a7b42
timofeykl <84674136+timofeykl@users.noreply.github.com>
2021-05-23 20:34:22 +0300

    Update XREF_CREDIT_PROGRAM_ALIAS.sql

--- XREF_CREDIT_PROGRAM_ALIAS.sql
+++ XREF_CREDIT_PROGRAM_ALIAS.sql
@@ -1,10 +1,10 @@
 select
 max(CREDIT_PROGRAM_ID),
-(lower(SUBSTR(replace(REGEXP_REPLACE(CREDIT_PROGRAM_ALIAS_NM, '_| ', ''), '.', ''), 0,
-length(replace(REGEXP_REPLACE(CREDIT_PROGRAM_ALIAS_NM, '_| ', ''), '.', '')) - 8)))
+(lower(SUBSTR(replace(REGEXP_REPLACE(CREDIT_PROGRAM, '_| ', ''), '.', ''), 0,
+length(replace(REGEXP_REPLACE(CREDIT_PROGRAM, '_| ', ''), '.', '')) - 8)))
 
-as  CREDIT_PROGRAM_ALIAS_NM_SPACEOUT
-from dm_sm.xref_credit_program_alias
+as  CREDIT_PROGRAM
+from smth
  group by
- (lower(SUBSTR(replace(REGEXP_REPLACE(CREDIT_PROGRAM_ALIAS_NM, '_| ', ''), '.', ''), 0,
- length(replace(REGEXP_REPLACE(CREDIT_PROGRAM_ALIAS_NM, '_| ', ''), '.', '')) - 8)))
+ (lower(SUBSTR(replace(REGEXP_REPLACE(CREDIT_PROGRAM, '_| ', ''), '.', ''), 0,
+ length(replace(REGEXP_REPLACE(CREDIT_PROGRAM, '_| ', ''), '.', '')) - 8)))
